8696119144148c4805a22ba528ce90192316ab76
Tr0sT <tr0st@bk.ru>
2020-05-23 16:00:43 +0300

    chore: fix upm branching?

---
+++ publish_upm.sh
@@ -0,0 +1,8 @@
+BRANCH=$1
+TAG=$2
+git subtree split --prefix=Packages/com.nuclearband.sodatabase -b $BRANCH
+#git filter-branch --prune-empty --tree-filter 'rm -rf Tests' upm
+git gc
+git filter-repo --force --invert-paths --path Samples.meta --path-rename "Samples:Samples~" --refs $BRANCH
+git tag $TAG $BRANCH
+git push origin $BRANCH --tags
