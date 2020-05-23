b8f7a0b8eba379cc24881773da77aa1e8c7ace4c
Tr0sT <tr0st@bk.ru>
2020-05-23 16:52:57 +0300

    fix: and again

--- publish_upm.sh
+++ publish_upm.sh
@@ -1,7 +1,7 @@
 BRANCH=$1
 TAG=$2
-git pull 
 git subtree split --prefix=Packages/com.nuclearband.sodatabase -b $BRANCH
+git pull -f
 #git filter-branch --prune-empty --tree-filter 'rm -rf Tests' upm
 git gc
 git filter-repo --force --invert-paths --path Samples.meta --path-rename "Samples:Samples~" --refs $BRANCH
