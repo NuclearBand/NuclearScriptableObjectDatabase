d15ff5399f42402ad6c3b94dfe057db6f9282829
serg.morozov <serg.morozov@vk.team>
2023-02-04 15:45:21 +0300

    fix: SODatabaseInternal back to publick

--- Assets/com.nuclearband.sodatabase/Runtime/SODatabaseUtilities.cs
+++ Assets/com.nuclearband.sodatabase/Runtime/SODatabaseUtilities.cs
@@ -8,16 +8,16 @@ using UnityEngine;
 
 namespace Nuclear.SODatabase
 {
-    internal static class SODatabaseUtilities
+    public static class SODatabaseUtilities
     {
         // ReSharper disable once MemberCanBePrivate.Global
-        internal static T GetModelForEdit<T>(string path) where T : DataNode
+        public static T GetModelForEdit<T>(string path) where T : DataNode
         {
             return AssetDatabase.LoadAssetAtPath<T>(SODatabaseSettings.Path + path + ".asset") ??
                 throw new ArgumentException($"Could not get model at path {path}");
         }
 
-        internal static List<T> GetModelsForEdit<T>(string path) where T : DataNode
+        public static List<T> GetModelsForEdit<T>(string path) where T : DataNode
         {
             var searchPath = (SODatabaseSettings.Path + path).TrimEnd('/');
             var modelGUIDs = AssetDatabase.FindAssets($"t:{typeof(T).Name}",new [] {searchPath});
@@ -26,7 +26,7 @@ namespace Nuclear.SODatabase
         }
 
         // ReSharper disable once MemberCanBePrivate.Global
-        internal static void CreateFolder(string path)
+        public static void CreateFolder(string path)
         {
             AssetDatabase.Refresh();
             var folders = path.Split('/');
@@ -45,7 +45,7 @@ namespace Nuclear.SODatabase
             AssetDatabase.SaveAssets();
         }
 
-        internal static T CreateModel<T>(string path, string name) where T : DataNode
+        public static T CreateModel<T>(string path, string name) where T : DataNode
         {
             try
             {
@@ -67,7 +67,7 @@ namespace Nuclear.SODatabase
             return obj;
         }
 
-        internal static void RemoveFolder(string path)
+        public static void RemoveFolder(string path)
         {
             AssetDatabase.DeleteAsset(SODatabaseSettings.Path + "/" + path);
             AssetDatabase.Refresh();
